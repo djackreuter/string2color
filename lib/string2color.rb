f9a20db1593b57e7c479bb6c0f9d706fd30d8916
Jack Reuter <djreuter45@gmail.com>
2018-01-11 15:05:29 -0700

    started adding some methods to convert string to int values

--- lib/string2color.rb
+++ lib/string2color.rb
@@ -1,5 +1,28 @@
-require "string2color/version"
-
+require_relative 'string2color/version'
+# change string to color
 module String2color
-  # Your code goes here...
+  # Class for color
+  class Color < String
+    attr_accessor :val_1, :val_2, :val_3
+
+    def store_string(str)
+      string = str
+    end
+
+    def first_val(string)
+      @val_1 = string.size
+      if @val_1 <= 10
+        @val_1 * 9
+      elsif @val_1 > 10 && @val_1 < 90
+        @val_1 * 2
+      end
+    end
+
+    def second_val(string)
+        
+    end
+  end
 end
+
+test = String2color::Color.new
+puts test.first_val('test')
