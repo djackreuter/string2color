b9aa67bd4f2b7c8eb9a593304ca9aac9613bc3dc
Jack Reuter <djreuter45@gmail.com>
2018-01-12 09:46:00 -0700

    wrote unit test for method

--- test/string2color_test.rb
+++ test/string2color_test.rb
@@ -1,11 +1,12 @@
-require "test_helper"
+require_relative 'test_helper'
+require_relative '../lib/string2color.rb'
 
 class String2colorTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::String2color::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_to_color
+    assert_equal 'RGB(54, 111, 102)', 'foobar'.to_color
   end
 end
