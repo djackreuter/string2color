3d1c648b280c0d0031ff6f8e4b8aa8c2d2d59a28
Jack Reuter <djreuter45@gmail.com>
2018-01-12 09:26:48 -0700

    did some refactoring and added to_color method on String class

--- lib/string2color.rb
+++ lib/string2color.rb
@@ -1,42 +1,27 @@
 require_relative 'string2color/version'
 # change string to color
-module String2color
-  # Class for color
-  class String
+# Add on to string class
+class String
+  def to_color
+    "RGB(#{val_1}, #{val_2}, #{val_3})"
+  end
 
-    def store_string(str)
-      string = str
+  def val_1
+    size
+    if size <= 10
+      size * 9
+    elsif size > 10 && size <= 90
+      size * 2
     end
+  end
 
-    def first_val(string)
-      @val_1 = string.size
-      if @val_1 <= 10
-        @val_1 * 9
-      elsif @val_1 > 10 && @val_1 <= 90
-        @val_1 * 2
-      end
-    end
+  def val_2
+    self[2].ord
+  end
 
-    def second_val(string)
-      @val_2 = string[3].ord
-    end
-
-    def third_val(string)
-      @val_3 = string[/./].ord
-    end
-
-    def to_color
-      "RGB(#{self.val_1}, #{val_2}, #{val_3})"
-    end
-    def val_1
-      self.
-    end
+  def val_3
+    self[/./].ord
   end
 end
 
-test = String2color::Color.new
-puts test.first_val('foobar')
-puts test.second_val('foobar')
-puts test.third_val('foobar')
-
-puts test.output
+puts 'foobar'.to_color
